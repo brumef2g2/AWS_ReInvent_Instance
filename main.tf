9ece81a640b02bd19250d1626804426ebe814529
nehrman <nicolas.ehrman@gmail.com>
2019-12-03 10:25:51 -0800

    Updating main.tf

--- main.tf
+++ main.tf
@@ -69,7 +69,7 @@ resource "random_id" "app-server-id" {
 module "aws_record_hashicat" {
   source        = "app.terraform.io/re-Invent/route53-records/aws"
   zone_id       = "${data.terraform_remote_state.base_layer.outputs.dns_zone_id}"
-  instance_name = ["${random_id.app-server-id.*.hex}}"]
+  instance_name = ["${random_id.app-server-id.hex}}"]
   instance_ip   = ["${module.aws_instance_hashicat.instance_public_ip}"]
   record_type   = "A"
   record_ttl    = "300"
